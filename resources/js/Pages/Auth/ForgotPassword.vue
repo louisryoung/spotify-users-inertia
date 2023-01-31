e5855eb80f7a002d60bd6b7c0685070620604a40
Louis Young <cool.young0420@gmail.com>
2023-01-31 14:24:39 +0100

    Install prettier and eslint

--- resources/js/Pages/Auth/ForgotPassword.vue
+++ resources/js/Pages/Auth/ForgotPassword.vue
@@ -8,54 +8,48 @@ import JetLabel from '@/Jetstream/Label.vue';
 import JetValidationErrors from '@/Jetstream/ValidationErrors.vue';
 
 defineProps({
-    status: String,
+  status: String,
 });
 
 const form = useForm({
-    email: '',
+  email: '',
 });
 
 const submit = () => {
-    form.post(route('password.email'));
+  form.post(route('password.email'));
 };
 </script>
 
 <template>
-    <Head title="Forgot Password" />
+  <Head title="Forgot Password" />
 
-    <JetAuthenticationCard>
-        <template #logo>
-            <JetAuthenticationCardLogo />
-        </template>
+  <JetAuthenticationCard>
+    <template #logo>
+      <JetAuthenticationCardLogo />
+    </template>
 
-        <div class="mb-4 text-sm text-gray-600">
-            Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.
-        </div>
+    <div class="mb-4 text-sm text-gray-600">
+      Forgot your password? No problem. Just let us know your email address and we will email you a password reset link
+      that will allow you to choose a new one.
+    </div>
 
-        <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
-            {{ status }}
-        </div>
+    <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
+      {{ status }}
+    </div>
 
-        <JetValidationErrors class="mb-4" />
+    <JetValidationErrors class="mb-4" />
 
-        <form @submit.prevent="submit">
-            <div>
-                <JetLabel for="email" value="Email" />
-                <JetInput
-                    id="email"
-                    v-model="form.email"
-                    type="email"
-                    class="mt-1 block w-full"
-                    required
-                    autofocus
-                />
-            </div>
+    <form @submit.prevent="submit">
+      <div>
+        <JetLabel for="email" value="Email" />
+        <JetInput id="email" v-model="form.email" type="email" class="mt-1 block w-full" required autofocus />
+      </div>
 
-            <div class="flex items-center justify-end mt-4">
-                <JetButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Email Password Reset Link
-                </JetButton>
-            </div>
-        </form>
-    </JetAuthenticationCard>
+      <div class="flex items-center justify-end mt-4">
+        <JetButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+          Email Password Reset Link
+        </JetButton>
+      </div>
+    </form>
+  </JetAuthenticationCard>
 </template>
--- resources/js/app.js
+++ resources/js/app.js
@@ -7,14 +7,14 @@ import { InertiaProgress } from '@inertiajs/progress';
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
-    resolve: (name) => require(`./Pages/${name}.vue`),
-    setup({ el, app, props, plugin }) {
-        return createApp({ render: () => h(app, props) })
-            .use(plugin)
-            .mixin({ methods: { route } })
-            .mount(el);
-    },
+  title: (title) => `${title} - ${appName}`,
+  resolve: (name) => require(`./Pages/${name}.vue`),
+  setup({ el, app, props, plugin }) {
+    return createApp({ render: () => h(app, props) })
+      .use(plugin)
+      .mixin({ methods: { route } })
+      .mount(el);
+  },
 });
 
 InertiaProgress.init({ color: '#4B5563' });
