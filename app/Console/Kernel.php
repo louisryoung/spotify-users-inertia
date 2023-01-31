e2c6c9a0fc4eb1b9bb23a8e5b090ad8955d96486
Louis Young <cool.young0420@gmail.com>
2023-01-31 18:51:10 +0100

    Add daily task to sync followers

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,13 +14,9 @@ class Kernel extends ConsoleKernel
      * @return void
      */
 
-    protected $commands = [
-        Commands\SyncFollowers::class
-    ];
-
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('sync:followers')->daily();
     }
 
     /**
